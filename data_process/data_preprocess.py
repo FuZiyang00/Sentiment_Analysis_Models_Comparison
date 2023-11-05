96419c260dc2604d278d03e05a1fff4907c78ddf
Fu Ziyang <fuziyang.fz@gmail.com>
2023-11-05 14:21:33 +0100

    EDA functions

--- data_process/data_preprocess.py
+++ data_process/data_preprocess.py
@@ -12,3 +12,23 @@ class EDA:
     def __init__(self, data):
         self.data = data
     
+    def dataset_info(self):
+        shape = self.data.shape
+        duplicates = self.data.duplicated().sum()
+        info = self.data.info()
+        return shape, duplicates, info
+    
+    def summary_statistics(self, column):
+        statistics = self.data[column].describe().round(2)
+        fig = px.histogram(self.data, x=column)
+        return statistics, fig 
+
+    def variables_relationship(self, column): 
+        self.data["reviews_lenght"] = self.data[column].apply(len)
+        line_plot = px.line(self.data, x="reviews_lenght",
+                             y=column, title='reviews lenght and rating')
+        scatter_plot = px.scatter(self.data, x="reviews_lenght",
+                                  y=column, color=column)
+        return line_plot, scatter_plot
+
+ 
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pandas as pd 
+from data_process.data_preprocess import EDA
 
 if __name__ == "__main__":
     reviews = pd.read_csv("tripadvisor_hotel_reviews.csv")
     print(reviews.head(10))
 
     print("Eploratory Data Analysis")
-    
+    eda = EDA(reviews)
+    shape, duplicates, info = eda.dataset_info()
+    print(shape)    
+    print(duplicates)
+    print(info)
+    column_name1 = "Rating"
+    statistics, figure = eda.summary_statistics(column_name1)
+    print(statistics)
