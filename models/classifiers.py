b4b7bab9148530112d5d50ebb805e009217c533f
Fu Ziyang <fuziyang.fz@gmail.com>
2023-11-21 16:08:13 +0100

    multiclassification models

---
+++ models/classifiers.py
@@ -0,0 +1,7 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+
+class classifiers: 
+    @staticmethod
+    def models_training(models, training_set):
+        pass
