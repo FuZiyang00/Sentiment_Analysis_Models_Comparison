087673381e4d04344e4cf09c8fb3a01a127f3e6b
Fu Ziyang <fuziyang.fz@gmail.com>
2023-12-05 22:22:08 +0100

    classifiers

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import pandas as pd 
-from data_process.data_preprocess import EDA
+from data_process.data_exploration import EDA
 from data_process.data_cleaner import Data_Cleaner
-from plotly.offline import plot
+from models.classifiers import classifiers
 from tqdm import tqdm
-import re, string
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+
 
 if __name__ == "__main__":
     df = pd.read_csv("tripadvisor_hotel_reviews.csv")
     ratings = "Rating"
     reviews = "Review"
-    print(df.head(10), "\n")
+    # print(df.head(10), "\n")
     tqdm.pandas()
 
     print("Eploratory Data Analysis: ")
@@ -30,6 +35,30 @@ if __name__ == "__main__":
     
     df[reviews] = df[reviews].progress_apply(clean_text)
     print(df.head(10))
+
+    print("Training and testing")
+    x_train, x_test, y_train, y_test = train_test_split(df[reviews], df[ratings], test_size=0.2)
+
+    tfid = TfidfVectorizer()
+    # Fit and transform on the training set
+    with tqdm(total=len(x_train), desc="Fitting and transforming train set") as pbar:
+        train_tfid_matrix = tfid.fit_transform(x_train)
+        pbar.update(len(x_train))
+
+    # Transform on the test set
+    with tqdm(total=len(x_test), desc="Transforming test set") as pbar:
+        test_tfid_matrix = tfid.transform(x_test)
+        pbar.update(len(x_test))
+    
+    print("training the classifiers")
+    svm_model = SVC()
+    dt_model = DecisionTreeClassifier()
+    knn_model = KNeighborsClassifier()
+
+    classification = classifiers(svm_model, dt_model, knn_model)
+    classification.models_training(train_tfid_matrix, y_train)
+    classification.model_evaluation(test_tfid_matrix, y_test)
+
 
 
     
--- models/classifiers.py
+++ models/classifiers.py
@@ -1,7 +1,28 @@
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+from tqdm import tqdm
 
 class classifiers: 
-    @staticmethod
-    def models_training(models, training_set):
-        pass
+    def __init__(self, *args):
+        self.models = list(args)
+
+    def models_training(self, x_train, y_train, batch_size=100):
+        for model in self.models:
+            # Create a tqdm progress bar for the current model
+            with tqdm(total=x_train.shape[0], desc=f"Training {str(model)}") as pbar:
+                for i in range(0, x_train.shape[0], batch_size):
+                    end_idx = min(i + batch_size, x_train.shape[0])
+                    model.fit(x_train[i:end_idx], y_train[i:end_idx])
+                    pbar.update(end_idx - i)
+    
+    def model_evaluation(self, x_test, y_test):
+        for model in self.models:
+            y_pred = model.predict(x_test)
+            print(model,":",accuracy_score(y_test,y_pred)*100)
+
+
+
+    
+
+        
+        
