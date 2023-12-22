b6d74a85d70ecbcdef850e8e6db011be6fdca251
Fu Ziyang <fuziyang.fz@gmail.com>
2023-12-23 00:11:55 +0100

    polymorphism

--- models/lstm.py
+++ models/lstm.py
@@ -2,18 +2,9 @@ from keras import layers
 from keras.models import Sequential 
 from keras.optimizers import Adam
 from keras.metrics import AUC
-from sklearn.metrics import classification_report
+from .base_model import BaseModel
 
-class LSTM: 
-
-    def __init__(self, X_train, y_train, X_val, y_val, X_test, y_test):
-        self.X_train = X_train
-        self.X_test = X_test
-        self.X_val = X_val
-        self.y_train = y_train
-        self.y_test = y_test
-        self.y_val = y_val
-        self.model = self.model_creation()
+class LSTM(BaseModel): 
 
     def model_creation(self):
         model = Sequential([])
@@ -29,11 +20,5 @@ class LSTM:
         
         return model 
     
-    def model_training(self, class_weights):
-        self.model.fit(self.X_train, self.y_train, 
-                  validation_data=(self.X_val, self.y_val), 
-                  epochs=5, class_weight=class_weights)
     
-    def model_evaluation(self):
-        test_predictions = self.model.predict(self.X_test)
-        print(classification_report(self.y_test, test_predictions))
+
--- models/rnn.py
+++ models/rnn.py
@@ -3,18 +3,9 @@ from keras.optimizers import Adam
 from keras.metrics import AUC
 from keras.models import Sequential
 from sklearn.metrics import classification_report
-from .lstm import LSTM
+from .base_model import BaseModel
 
-class RNN(LSTM):
-
-    def __init__(self, X_train, y_train, X_val, y_val, X_test, y_test):
-        self.X_train = X_train
-        self.X_test = X_test
-        self.X_val = X_val
-        self.y_train = y_train
-        self.y_test = y_test
-        self.y_val = y_val
-        self.model = self.model_creation()
+class RNN(BaseModel):
 
     def model_creation(self):
         model = Sequential([])
@@ -27,12 +18,3 @@ class RNN(LSTM):
               metrics=['accuracy', AUC(name='auc')])
         
         return model 
-    
-    def model_training(self, class_weights):
-        self.model.fit(self.X_train, self.y_train, 
-                  validation_data=(self.X_val, self.y_val), 
-                  epochs=5, class_weight=class_weights)
-    
-    def model_evaluation(self):
-        test_predictions = self.model.predict(self.X_test)
-        print(classification_report(self.y_test, test_predictions))
